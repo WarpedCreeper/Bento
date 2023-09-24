bcb8f1c6e587af87c1b0a121cc8f12faab56da0a
Warped Creeper <118295839+WarpedCreeper@users.noreply.github.com>
2023-09-24 11:06:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -65,9 +65,9 @@ const CONFIG = {
 		},
 		{
 			id: '3',
-			name: 'Todoist',
+			name: 'Dzen',
 			icon: 'trello',
-			link: 'https://todoist.com',
+			link: 'https://dzen.ru',
 		},
 		{
 			id: '4',
@@ -83,9 +83,9 @@ const CONFIG = {
 		},
 		{
 			id: '6',
-			name: 'Odysee',
+			name: 'YouTube',
 			icon: 'youtube',
-			link: 'https://odysee.com/',
+			link: 'https://youtube.com/',
 		},
 	],
 
